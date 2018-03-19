cb6a4890880475a42177529f69528ed168a9e10f
Lucy Wall <lhwall@gmail.com>
2018-03-19 18:14:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+ if speed < 60 && speed > 40
+   return false 
+ else return true 
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+speed < 60 && speed > 40 ? false : true
 end
 	
 
